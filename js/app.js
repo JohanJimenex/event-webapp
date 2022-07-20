affffff455cf7eca401a87d4ba2934a77058395b
Johan Jiménez <johan.jimenez.23@gmail.com>
2022-07-20 01:15:01 -0400

    bootstrap, animaciones, peticiones agregados

--- js/app.js
+++ js/app.js
@@ -1,79 +1,81 @@
-const container = document.querySelector("#container-cards");
+const container = document.querySelector("#col-container-cards");
 
 const urlBase = "https://goldfish-app-oewvc.ondigitalocean.app/api/v1";
 
-let arrEventos = [1];
+let arrEventos = [];
 
 // esta función es anonima sin nombre tipo flecha, se autollama ella misma.
 (async () => {
 
-    let request = await fetch(urlBase + "/events");
+    let request = await fetch(urlBase + "/event");
     let response = await request.json();
 
-    arrEventos = response.users
+    arrEventos = response.events
+
+    for (let event of arrEventos) {
+
+        container.innerHTML += `
+            <div class="col col-md-3 mb-3 animate__animated animate__backInUp d-flex justify-content-center" >
+                <div class="card" style="width: 18rem;">
+                    <img src="${event.image_url}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title text-center">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <div class="row mx-2">
+                        <button href="#" class="btn btn-primary col-5">ADQUIRIR</button>
+                        <small class="col-7" > Fecha: ${event.date}</small>
+                    </div>
+                    </div>
+                </div>
+            </div>  
+        `
+    }
+
 })()
 
 
-for (const event in arrEventos) {
-
-    container.innerHTML += `
-    
-        <div class="card">
-            <img src="${event.img}" alt="evento img">
-
-            <h3>${event.name}</h3>
-            <p class="texto-card">${event.desc} </p>
-
-            <p class="fecha">FECHA: ${event.date}</p>
-            <a href="#" class="btn-adquirir">ADQUIRIR</a>
-        </div>
-    
-    `
-}
 
 
 
-class Usuario {
+// class Usuario {
 
-    firstname
-    lastname
-    email
-    phone
-    birthday
-    password
-    created_at
-    updated_at
+//     firstname
+//     lastname
+//     email
+//     phone
+//     birthday
+//     password
+//     created_at
+//     updated_at
 
-    constructor(firstname, lastname, email, phone, birthday, password) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.phone = phone;
-        this.birthday = birthday;
-        this.password = password;
-        this.created_at = new Date();
-        this.updated_at = new Date();
+//     constructor(firstname, lastname, email, phone, birthday, password) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.email = email;
+//         this.phone = phone;
+//         this.birthday = birthday;
+//         this.password = password;
+//         this.created_at = new Date();
+//         this.updated_at = new Date();
 
-    }
-}
-
-
-async function crearUsuario() {
-
-    let x = new Usuario();
-
-    options = {
-        method: "POST",
-        body: JSON.stringify(usuario)
-    }
-
-    let request = await fetch(urlBase + "/user", options);
-    let response = await request.json();
-
-    console.log(response);
-}
+//     }
+// }
 
 
+// async function crearUsuario() {
+
+//     let x = new Usuario();
+
+//     options = {
+//         method: "POST",
+//         body: JSON.stringify(usuario)
+//     }
+
+//     let request = await fetch(urlBase + "/user", options);
+//     let response = await request.json();
+
+//     console.log(response);
+// }
 
 
 
